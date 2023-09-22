7c843160c5904fcd9f6b3b6e7e530093282f9e91
MrDjeb <MrDjeb245@gmail.com>
2023-09-22 14:58:04 +0300

    step_1 done, tempalate clean arch

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-park-mail-ru/2023_2_potatiki
+
+go 1.21.0
---
+++ internal/pkg/utils/response/response.go
@@ -0,0 +1,24 @@
+package response
+
+type Response struct {
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+}
+
+const (
+	StatusOK    = "OK"
+	StatusError = "Error"
+)
+
+func OK() Response {
+	return Response{
+		Status: StatusOK,
+	}
+}
+
+func Error(msg string) Response {
+	return Response{
+		Status: StatusError,
+		Error:  msg,
+	}
+}
