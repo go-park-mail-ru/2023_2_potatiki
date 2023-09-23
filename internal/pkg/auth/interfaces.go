b678f8813860e94b9b0d0cd275a54f0b18115951
Khristina <hristinakirillova2559@gmail.com>
2023-09-23 22:22:39 +0300

    add mocks

---
+++ internal/pkg/auth/interfaces.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/go-park-mail-ru/2023_2_potatiki/internal/models"
+	"github.com/google/uuid"
+)
+
+//go:generate mockgen -source=interfaces.go -destination=./mocks/auth_mock.go -package=mock
+
+type AuthUsecase interface {
+	Login(context.Context, models.User) (models.Profile, error)
+	Singup(context.Context, models.User) (models.Profile, error)
+	GetProfile(context.Context, uuid.UUID) (models.Profile, error)
+}
+
+type AuthRepo interface {
+	CreateUser(context.Context, models.User) (models.Profile, error)
+	CheckUser(context.Context, models.User) (models.Profile, error)
+	ReadProfile(context.Context, uuid.UUID) (models.Profile, error)
+}
---
+++ internal/pkg/products/interfaces.go
@@ -0,0 +1,20 @@
+package products
+
+import (
+	"context"
+
+	"github.com/go-park-mail-ru/2023_2_potatiki/internal/models"
+	"github.com/google/uuid"
+)
+
+//go:generate mockgen -source=interfaces.go -destination=./mocks/products_mock.go -package=mock
+
+type ProductUsecase interface {
+	GetProduct(context.Context, uuid.UUID) (models.Product, error)
+	GetProducts(context.Context, int64, int64) ([]models.Product, error)
+}
+
+type ProductRepo interface {
+	ReadProduct(context.Context, uuid.UUID) (models.Product, error)
+	ReadProducts(context.Context, int64, int64) ([]models.Product, error)
+}
