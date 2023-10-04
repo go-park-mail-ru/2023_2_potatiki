a05ae53a9e016135996c9d5d59b9a138e058e8c4
Belozerovmsk <belozerov.msk@gmail.com>
2023-10-04 12:08:18 +0300

    New tests added

--- internal/pkg/auth/delivery/http/handlers_test.go
+++ internal/pkg/auth/delivery/http/handlers_test.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -28,5 +30,71 @@ func TestSignUp(t *testing.T) {
 	w := httptest.NewRecorder()
 	AuthHandler := NewAuthHandler(logger.Set("prod"), uc)
 	AuthHandler.SignUp(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestSignIn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	uc := mock.NewMockAuthUsecase(ctrl)
+	uc.EXPECT().SignIn(gomock.Any(), models.User{
+		Login:        "User",
+		PasswordHash: "Dima@gmail.com",
+	}).Return(models.Profile{}, "", time.Time{}, nil)
+
+	req := httptest.NewRequest(http.MethodPost, "http://example.com/foo",
+		strings.NewReader("{ \"login\": \"User\", \"password\": \"Dima@gmail.com\" }"))
+	w := httptest.NewRecorder()
+	AuthHandler := NewAuthHandler(logger.Set("prod"), uc)
+	AuthHandler.SignIn(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestLogOut(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	uc := mock.NewMockAuthUsecase(ctrl)
+
+	req := httptest.NewRequest(http.MethodGet, "http://example.com/foo", nil)
+	w := httptest.NewRecorder()
+	AuthHandler := NewAuthHandler(logger.Set("prod"), uc)
+	AuthHandler.LogOut(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestCheckAuth(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	uc := mock.NewMockAuthUsecase(ctrl)
+	uc.EXPECT().CheckToken(gomock.Any(), gomock.Any()).Return(uuid.New(), nil)
+
+	req := httptest.NewRequest(http.MethodGet, "http://example.com/foo", nil)
+	cookie := &http.Cookie{
+		Name:  AccessTokenCookieName,
+		Value: "your_token_here",
+	}
+	req.AddCookie(cookie)
+
+	w := httptest.NewRecorder()
+	AuthHandler := NewAuthHandler(logger.Set("prod"), uc)
+	AuthHandler.CheckAuth(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestGetProfile(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	uc := mock.NewMockAuthUsecase(ctrl)
+	idProfile := uuid.New()
+
+	uc.EXPECT().GetProfile(gomock.Any(), idProfile).Return(models.Profile{}, nil)
+
+	req := httptest.NewRequest(http.MethodGet, "http://example.com/foo", nil)
+	req = mux.SetURLVars(req, map[string]string{"id": idProfile.String()}) // Устанавливаем параметр "id" в URL.
+	w := httptest.NewRecorder()
+	AuthHandler := NewAuthHandler(logger.Set("prod"), uc)
+	AuthHandler.GetProfile(w, req)
+
 	assert.Equal(t, http.StatusOK, w.Code)
 }
