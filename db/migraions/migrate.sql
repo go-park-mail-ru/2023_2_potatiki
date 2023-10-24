605ed2ec43bc166eb31c919f81c4595784d52a64
khristina455 <91967143+khristina455@users.noreply.github.com>
2023-10-24 03:58:56 +0300

    add  constraints

--- db/migraions/migrate.sql
+++ db/migraions/migrate.sql
@@ -53,18 +53,19 @@ CREATE TABLE IF NOT EXISTS promocode
 (
     id UUID NOT NULL PRIMARY KEY,
     discount INT NOT NULL,
-    name TEXT UNIQUE
+    name TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS order_info
 (
     id UUID NOT NULL PRIMARY KEY,
     delivery_date TIMESTAMP,
-    creation_date TIMESTAMP,
-    profile_id UUID,
-    status TEXT,
+    creation_date TIMESTAMP NOT NULL,
+    profile_id UUID NOT NULL,
+    status TEXT NOT NULL,
     FOREIGN KEY (profile_id) REFERENCES profile(id) ON DELETE CASCADE,
     promocode_id UUID,
+    CONSTRAINT "ProfilePromocode_unique" UNIQUE (profile_id, promocode_id),
     FOREIGN KEY (promocode_id) REFERENCES promocode(id) ON DELETE RESTRICT
 );
 
@@ -72,10 +73,11 @@ CREATE TABLE IF NOT EXISTS order_info
 CREATE TABLE IF NOT EXISTS order_item
 (
     id UUID NOT NULL PRIMARY KEY,
-    order_id UUID,
+    order_id UUID NOT NULL,
     FOREIGN KEY (order_id) REFERENCES order_info(id) ON DELETE CASCADE,
-    product_id UUID,
+    product_id UUID NOT NULL,
     FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE,
+    CONSTRAINT "OrderProduct_unique" UNIQUE (order_id, product_id),
     quantity INT,
     CHECK (quantity >= 0)
 );
@@ -86,10 +88,11 @@ CREATE TABLE IF NOT EXISTS address
     id UUID NOT NULL PRIMARY KEY,
     profile_id UUID,
     FOREIGN KEY (profile_id) REFERENCES profile(id) ON DELETE CASCADE,
-    city TEXT,
-    street TEXT,
-    house TEXT,
-    flat TEXT,
+    city TEXT NOT NULL,
+    street TEXT NOT NULL,
+    house TEXT NOT NULL,
+    flat TEXT NOT NULL,
+    CONSTRAINT "Address_unique" UNIQUE (city, street, house, flat),
     is_current BOOLEAN
 );
 
@@ -98,6 +101,7 @@ CREATE TABLE IF NOT EXISTS category_reference
 (
     id UUID NOT NULL PRIMARY KEY,
     category_id UUID,
+    FOREIGN KEY (id) REFERENCES category(id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
 );
 
@@ -105,10 +109,11 @@ CREATE TABLE IF NOT EXISTS category_reference
 CREATE TABLE IF NOT EXISTS shopping_cart_item
 (
     id UUID NOT NULL PRIMARY KEY,
-    profile_id UUID,
+    profile_id UUID NOT NULL,
     FOREIGN KEY (profile_id) REFERENCES profile(id) ON DELETE CASCADE,
-    product_id UUID,
+    product_id UUID NOT NULL,
     FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE,
+    CONSTRAINT "ProfileProduct_unique" UNIQUE (profile_id, product_id),
     quantity INT,
     CHECK (quantity >= 0)
 );
@@ -116,8 +121,9 @@ CREATE TABLE IF NOT EXISTS shopping_cart_item
 CREATE TABLE IF NOT EXISTS favorite
 (
     id UUID NOT NULL PRIMARY KEY,
-    profile_id UUID,
+    profile_id UUID NOT NULL,
     FOREIGN KEY (profile_id) REFERENCES profile(id) ON DELETE CASCADE,
-    product_id UUID,
-    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
+    product_id UUID NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE,
+    CONSTRAINT "ProfileProduct_unique" UNIQUE (profile_id, product_id)
 );
