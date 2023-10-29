c31ad813ded0ee202c020d08518ec4fc0a1716c5
Ruslan Khalikov <rus-h-903@mail.ru>
2023-10-29 20:36:10 +0300

    add handles to cart

--- internal/pkg/cart/delivery/http/handlers.go
+++ internal/pkg/cart/delivery/http/handlers.go
@@ -27,59 +27,6 @@ func NewCartHandler(log *slog.Logger, uc cart.CartUsecase) CartHandler {
 	}
 }
 
-// @Summary	UpdateCart
-// @Tags Cart
-// @Description	Update cart
-// @Accept json
-// @Produce json
-// @Param input body models.Cart true "cart info"
-// @Success	200	{object} models.Cart "cart info"
-// @Failure	400	{object} response.Response	"error messege"
-// @Failure	401
-// @Failure	429
-// @Router	/api/cart/update [post]
-func (h *CartHandler) UpdateCart(w http.ResponseWriter, r *http.Request) {
-	h.log = h.log.With(
-		slog.String("op", sl.GFN()),
-		slog.String("request_id", r.Header.Get(logmw.RequestIDCtx)),
-	)
-	userID, ok := r.Context().Value(authmw.AccessTokenCookieName).(uuid.UUID)
-	if !ok {
-		h.log.Error("failed cast uuid from context value")
-		resp.JSONStatus(w, http.StatusUnauthorized)
-
-		return
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if resp.BodyErr(err, h.log, w) {
-		return
-	}
-	defer r.Body.Close()
-	h.log.Debug("request body decoded", slog.Any("request", r))
-
-	c := models.Cart{}
-	err = json.Unmarshal(body, &c)
-	if err != nil {
-		h.log.Error("failed to unmarshal request body", sl.Err(err))
-		resp.JSONStatus(w, http.StatusTooManyRequests)
-
-		return
-	}
-	c.ProfileId = userID
-
-	cart, err := h.uc.UpdateCart(r.Context(), c)
-	if err != nil {
-		h.log.Error("failed to update cart", sl.Err(err))
-		resp.JSONStatus(w, http.StatusTooManyRequests)
-
-		return
-	}
-
-	h.log.Debug("update cart")
-	resp.JSON(w, http.StatusOK, cart)
-}
-
 // @Summary	GetCart
 // @Tags Cart
 // @Description	Get cart
@@ -112,5 +59,113 @@ func (h *CartHandler) GetCart(w http.ResponseWriter, r *http.Request) {
 	}
 
 	h.log.Debug("h.uc.GetCart", "cart", cart)
+	resp.JSON(w, http.StatusOK, cart)
+}
+
+// @Summary	AddProduct
+// @Tags Cart
+// @Description	add product to cart or change its number
+// @Accept json
+// @Produce json
+// @Param input body models.CartProduct true "product info"
+// @Success	200	{object} models.Cart "cart info"
+// @Failure	400	{object} response.Response	"error message"
+// @Failure	401
+// @Failure	429
+// @Router	/api/cart/add_product [post]
+func (h *CartHandler) AddProduct(w http.ResponseWriter, r *http.Request) {
+	h.log = h.log.With(
+		slog.String("op", sl.GFN()),
+		slog.String("request_id", r.Header.Get(logmw.RequestIDCtx)),
+	)
+	userID, ok := r.Context().Value(authmw.AccessTokenCookieName).(uuid.UUID)
+	if !ok {
+		h.log.Error("failed cast uuid from context value")
+		resp.JSONStatus(w, http.StatusUnauthorized)
+
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if resp.BodyErr(err, h.log, w) {
+		return
+	}
+	defer r.Body.Close()
+	h.log.Debug("request body decoded", slog.Any("request", r))
+
+	p := models.CartProduct{}
+	err = json.Unmarshal(body, &p)
+	if err != nil {
+		h.log.Error("failed to unmarshal request body", sl.Err(err))
+		resp.JSONStatus(w, http.StatusTooManyRequests)
+
+		return
+	}
+	c := models.Cart{}
+	c.ProfileId = userID
+
+	cart, err := h.uc.AddProduct(r.Context(), c, p)
+	if err != nil {
+		h.log.Error("failed to add product to cart", sl.Err(err))
+		resp.JSONStatus(w, http.StatusTooManyRequests)
+
+		return
+	}
+
+	h.log.Debug("add product to cart")
+	resp.JSON(w, http.StatusOK, cart)
+}
+
+// @Summary	DeleteProduct
+// @Tags Cart
+// @Description	delete product from cart
+// @Accept json
+// @Produce json
+// @Param input body models.CartProduct true "product info"
+// @Success	200	{object} models.Cart "cart info"
+// @Failure	400	{object} response.Response	"error message"
+// @Failure	401
+// @Failure	429
+// @Router	/api/cart/delete_product [post]
+func (h *CartHandler) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	h.log = h.log.With(
+		slog.String("op", sl.GFN()),
+		slog.String("request_id", r.Header.Get(logmw.RequestIDCtx)),
+	)
+	userID, ok := r.Context().Value(authmw.AccessTokenCookieName).(uuid.UUID)
+	if !ok {
+		h.log.Error("failed cast uuid from context value")
+		resp.JSONStatus(w, http.StatusUnauthorized)
+
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if resp.BodyErr(err, h.log, w) {
+		return
+	}
+	defer r.Body.Close()
+	h.log.Debug("request body decoded", slog.Any("request", r))
+
+	p := models.CartProduct{}
+	err = json.Unmarshal(body, &p)
+	if err != nil {
+		h.log.Error("failed to unmarshal request body", sl.Err(err))
+		resp.JSONStatus(w, http.StatusTooManyRequests)
+
+		return
+	}
+	c := models.Cart{}
+	c.ProfileId = userID
+
+	cart, err := h.uc.DeleteProduct(r.Context(), c, p)
+	if err != nil {
+		h.log.Error("failed to delete product from cart", sl.Err(err))
+		resp.JSONStatus(w, http.StatusTooManyRequests)
+
+		return
+	}
+
+	h.log.Debug("delete product from cart")
 	resp.JSON(w, http.StatusOK, cart)
 }
