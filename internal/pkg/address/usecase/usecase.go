6263a0b9d0d128c4d5ddc94611b27fcca7968c5f
MrDjeb <MrDjeb245@gmail.com>
2023-11-08 11:09:13 +0300

    cart usecase test

--- internal/pkg/address/usecase/usecase.go
+++ internal/pkg/address/usecase/usecase.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+
 	"github.com/go-park-mail-ru/2023_2_potatiki/internal/models"
 	"github.com/go-park-mail-ru/2023_2_potatiki/internal/pkg/address"
 	"github.com/go-park-mail-ru/2023_2_potatiki/internal/pkg/address/repo"
@@ -25,22 +26,16 @@ func (uc *AddressUsecase) AddAddress(ctx context.Context, userID uuid.UUID, addr
 
 	address, err := uc.repo.CreateAddress(ctx, userID, addressInfo)
 	if err != nil {
-		err = fmt.Errorf("error happened in repo.CreateAddress: %w", err)
-
-		return models.Address{}, err
+		return models.Address{}, fmt.Errorf("error happened in repo.CreateAddress: %w", err)
 	}
-
 	return address, nil
 }
 
 func (uc *AddressUsecase) UpdateAddress(ctx context.Context, addressInfo models.Address) (models.Address, error) {
 	addressInfo.Sanitize()
 
-	err := uc.repo.UpdateAddress(ctx, addressInfo)
-	if err != nil {
-		err = fmt.Errorf("error happened in repo.UpdateAddress: %w", err)
-
-		return models.Address{}, err
+	if err := uc.repo.UpdateAddress(ctx, addressInfo); err != nil {
+		return models.Address{}, fmt.Errorf("error happened in repo.UpdateAddress: %w", err)
 	}
 
 	address, err := uc.repo.ReadAddress(ctx, addressInfo.ProfileId, addressInfo.Id)
@@ -48,39 +43,29 @@ func (uc *AddressUsecase) UpdateAddress(ctx context.Context, addressInfo models.
 		if errors.Is(err, repo.ErrAddressNotFound) {
 			return models.Address{}, err
 		}
-		err = fmt.Errorf("error happened in repo.ReadAddress: %w", err)
-
-		return models.Address{}, err
+		return models.Address{}, fmt.Errorf("error happened in repo.ReadAddress: %w", err)
 	}
 
 	return address, nil
 }
 
 func (uc *AddressUsecase) DeleteAddress(ctx context.Context, addressInfo models.AddressDelete) error {
-	err := uc.repo.DeleteAddress(ctx, addressInfo)
-	if err != nil {
+	if err := uc.repo.DeleteAddress(ctx, addressInfo); err != nil {
 		if errors.Is(err, repo.ErrNoCurrentAddressNotFound) {
 			return err
 		}
-		err = fmt.Errorf("error happened in repo.DeleteAddress: %w", err)
-
-		return err
+		return fmt.Errorf("error happened in repo.DeleteAddress: %w", err)
 	}
-
 	return nil
 }
 
 func (uc *AddressUsecase) MakeCurrentAddress(ctx context.Context, addressInfo models.AddressMakeCurrent) error {
-	err := uc.repo.MakeCurrentAddress(ctx, addressInfo)
-	if err != nil {
+	if err := uc.repo.MakeCurrentAddress(ctx, addressInfo); err != nil {
 		if errors.Is(err, repo.ErrCurrentAddressNotFound) {
 			return err
 		}
-		err = fmt.Errorf("error happened in repo.MakeCurrentAddress: %w", err)
-
-		return err
+		return fmt.Errorf("error happened in repo.MakeCurrentAddress: %w", err)
 	}
-
 	return nil
 }
 
@@ -90,11 +75,8 @@ func (uc *AddressUsecase) GetCurrentAddress(ctx context.Context, userID uuid.UUI
 		if errors.Is(err, repo.ErrAddressNotFound) {
 			return models.Address{}, err
 		}
-		err = fmt.Errorf("error happened in repo.ReadCurrentAddress: %w", err)
-
-		return models.Address{}, err
+		return models.Address{}, fmt.Errorf("error happened in repo.ReadCurrentAddress: %w", err)
 	}
-
 	return address, nil
 }
 
@@ -104,10 +86,7 @@ func (uc *AddressUsecase) GetAllAddresses(ctx context.Context, userID uuid.UUID)
 		if errors.Is(err, repo.ErrAddressesNotFound) {
 			return []models.Address{}, err
 		}
-		err = fmt.Errorf("error happened in repo.ReadAllAddresses: %w", err)
-
-		return []models.Address{}, err
+		return []models.Address{}, fmt.Errorf("error happened in repo.ReadAllAddresses: %w", err)
 	}
-
 	return address, nil
 }
