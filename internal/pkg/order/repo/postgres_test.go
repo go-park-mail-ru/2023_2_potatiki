68a20bd6ca47dea78fcb1cf898247a719824c404
Ruslan Khalikov <rus-h-903@mail.ru>
2023-11-29 15:53:45 +0300

    order repo test done

--- internal/pkg/order/repo/postgres_test.go
+++ internal/pkg/order/repo/postgres_test.go
@@ -2,7 +2,10 @@ package repo
 
 import (
 	"context"
-	"reflect"
+	"github.com/driftprogramming/pgxpoolmock"
+	"github.com/golang/mock/gomock"
+	"github.com/jackc/pgx/v4"
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"github.com/go-park-mail-ru/2023_2_potatiki/internal/models"
@@ -10,116 +13,188 @@ import (
 )
 
 func TestOrderRepo_CreateOrder(t *testing.T) {
-	type args struct {
-		ctx       context.Context
-		cart      models.Cart
-		addressID uuid.UUID
-		userID    uuid.UUID
-		statusID  int64
-	}
+	addressID := uuid.NewV4()
+	userID := uuid.NewV4()
+	var statusID int64 = 0
 	tests := []struct {
-		name    string
-		r       *OrderRepo
-		args    args
-		want    models.Order
-		wantErr bool
-	}{
-		// TODO: Add test cases.
+		name       string
+		mockRepoFn func(pool *pgxpoolmock.MockPgxPool)
+		err        error
+	}{{
+		name: "SuccessfullCreateOrder",
+		mockRepoFn: func(mockPool *pgxpoolmock.MockPgxPool) {
+			mockPool.EXPECT().Exec(
+				gomock.Any(),
+				createOrder,
+				gomock.Any(),
+				gomock.Any(),
+				gomock.Any(),
+				gomock.Any(),
+				gomock.Any()).Return(nil, nil)
+		},
+		err: nil,
+	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.r.CreateOrder(tt.args.ctx, tt.args.cart, tt.args.addressID, tt.args.userID, tt.args.statusID)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("OrderRepo.CreateOrder() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("OrderRepo.CreateOrder() = %v, want %v", got, tt.want)
-			}
+			ctr := gomock.NewController(t)
+			mockPool := pgxpoolmock.NewMockPgxPool(ctr)
+			defer ctr.Finish()
+
+			tt.mockRepoFn(mockPool)
+
+			repo := NewOrderRepo(mockPool)
+			_, err := repo.CreateOrder(context.Background(), models.Cart{}, addressID, userID, statusID)
+
+			assert.Equal(t, tt.err, err)
 		})
 	}
 }
 
 func TestOrderRepo_ReadOrderID(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		userID uuid.UUID
-	}
+	userID := uuid.NewV4()
 	tests := []struct {
-		name    string
-		r       *OrderRepo
-		args    args
-		want    uuid.UUID
-		wantErr bool
-	}{
-		// TODO: Add test cases.
+		name       string
+		mockRepoFn func(pool *pgxpoolmock.MockPgxPool, pgxRows pgx.Rows)
+		err        error
+		columns    []string
+	}{{
+		name: "SuccessfullReadOrderID",
+		mockRepoFn: func(mockPool *pgxpoolmock.MockPgxPool, pgxRows pgx.Rows) {
+			mockPool.EXPECT().QueryRow(
+				gomock.Any(),
+				getCurrentOrderID,
+				gomock.Any(),
+			).Return(pgxRows)
+			pgxRows.Next()
+		},
+		columns: []string{"order_id"},
+		err:     nil,
+	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.r.ReadOrderID(tt.args.ctx, tt.args.userID)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("OrderRepo.ReadOrderID() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("OrderRepo.ReadOrderID() = %v, want %v", got, tt.want)
-			}
+			ctr := gomock.NewController(t)
+			mockPool := pgxpoolmock.NewMockPgxPool(ctr)
+			defer ctr.Finish()
+
+			pgxRows := pgxpoolmock.NewRows(tt.columns).AddRow(uuid.UUID{}).ToPgxRows()
+			tt.mockRepoFn(mockPool, pgxRows)
+
+			repo := NewOrderRepo(mockPool)
+			_, err := repo.ReadOrderID(context.Background(), userID)
+
+			assert.Equal(t, tt.err, err)
 		})
 	}
 }
 
 func TestOrderRepo_ReadOrder(t *testing.T) {
-	type args struct {
-		ctx     context.Context
-		orderID uuid.UUID
-	}
+	userID := uuid.NewV4()
 	tests := []struct {
-		name    string
-		r       *OrderRepo
-		args    args
-		want    models.Order
-		wantErr bool
-	}{
-		// TODO: Add test cases.
+		name       string
+		mockRepoFn func(pool *pgxpoolmock.MockPgxPool, pgxRows pgx.Rows)
+		err        error
+		columns    []string
+	}{{
+		name: "SuccessfullReadOrder",
+		mockRepoFn: func(mockPool *pgxpoolmock.MockPgxPool, pgxRows pgx.Rows) {
+			mockPool.EXPECT().Query(
+				gomock.Any(),
+				getCurrentOrder,
+				gomock.Any(),
+			).Return(pgxRows, nil)
+		},
+		columns: []string{
+			"product_id",
+			"product_name",
+			"product_description",
+			"product_price",
+			"product_imgsrc",
+			"product_rating",
+			"product_quantity",
+			"category_id",
+			"category_name",
+			"status_id",
+			"address_id",
+			"address_city",
+			"address_street",
+			"address_house",
+			"address_flat",
+			"is_current",
+		},
+		err: nil,
+	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.r.ReadOrder(tt.args.ctx, tt.args.orderID)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("OrderRepo.ReadOrder() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("OrderRepo.ReadOrder() = %v, want %v", got, tt.want)
-			}
+			ctr := gomock.NewController(t)
+			mockPool := pgxpoolmock.NewMockPgxPool(ctr)
+			defer ctr.Finish()
+
+			pgxRows := pgxpoolmock.NewRows(tt.columns).AddRow(
+				uuid.UUID{},
+				"",
+				"",
+				int64(0),
+				"",
+				float32(0),
+				int64(0),
+				int64(0),
+				"",
+				int64(0),
+				uuid.UUID{},
+				"",
+				"",
+				"",
+				"",
+				true,
+			).ToPgxRows()
+			tt.mockRepoFn(mockPool, pgxRows)
+
+			repo := NewOrderRepo(mockPool)
+			_, err := repo.ReadOrder(context.Background(), userID)
+
+			assert.Equal(t, tt.err, err)
 		})
 	}
 }
 
 func TestOrderRepo_ReadOrdersID(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		userID uuid.UUID
-	}
+	userID := uuid.NewV4()
 	tests := []struct {
-		name    string
-		r       *OrderRepo
-		args    args
-		want    []uuid.UUID
-		wantErr bool
-	}{
-		// TODO: Add test cases.
+		name       string
+		mockRepoFn func(pool *pgxpoolmock.MockPgxPool, pgxRows pgx.Rows)
+		err        error
+		columns    []string
+	}{{
+		name: "SuccessfullReadOrdersID",
+		mockRepoFn: func(mockPool *pgxpoolmock.MockPgxPool, pgxRows pgx.Rows) {
+			mockPool.EXPECT().Query(
+				gomock.Any(),
+				getOrdersID,
+				gomock.Any(),
+			).Return(pgxRows, nil)
+		},
+		columns: []string{
+			"order_id",
+		},
+		err: nil,
+	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.r.ReadOrdersID(tt.args.ctx, tt.args.userID)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("OrderRepo.ReadOrdersID() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("OrderRepo.ReadOrdersID() = %v, want %v", got, tt.want)
-			}
+			ctr := gomock.NewController(t)
+			mockPool := pgxpoolmock.NewMockPgxPool(ctr)
+			defer ctr.Finish()
+
+			pgxRows := pgxpoolmock.NewRows(tt.columns).AddRow(uuid.UUID{}).ToPgxRows()
+			tt.mockRepoFn(mockPool, pgxRows)
+
+			repo := NewOrderRepo(mockPool)
+			_, err := repo.ReadOrdersID(context.Background(), userID)
+
+			assert.Equal(t, tt.err, err)
 		})
 	}
 }
