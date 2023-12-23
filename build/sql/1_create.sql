b9d3abe3c33141c9c4716cd86979bb49cd93c391
Belozerovmsk <belozerov.msk@gmail.com>
2023-12-23 19:18:53 +0300

    FIX: time bug in hub

--- build/sql/1_create.sql
+++ build/sql/1_create.sql
@@ -403,7 +403,7 @@ BEGIN
 
     IF NEW.id IS NOT NULL THEN
         INSERT INTO messages (user_id, created, message_info)
-        VALUES (NEW.id, CURRENT_TIMESTAMP + interval '5 seconds', 'Спасибо за регистрацию, мы дарим вам промокод: ZUZU10');
+        VALUES (NEW.id, CURRENT_TIMESTAMP + interval '7 seconds', 'Спасибо за регистрацию, мы дарим вам промокод: ZUZU10');
     END IF;
 
     RETURN NEW;
--- internal/pkg/notifications/delivery/http/handlers.go
+++ internal/pkg/notifications/delivery/http/handlers.go
@@ -20,12 +20,14 @@ var (
 type NotificationsHandler struct {
 	hub userConnections.HubInterface
 	log *slog.Logger
+	//uc  comments.CommentsNotifications
 }
 
 func NewNotificationsHandler(hub userConnections.HubInterface, log *slog.Logger) *NotificationsHandler {
 	return &NotificationsHandler{
 		log: log,
 		hub: hub,
+		//uc:  uc,
 	}
 }
 
@@ -54,3 +56,34 @@ func (h *NotificationsHandler) GetNotifications(w http.ResponseWriter, r *http.R
 
 	h.log.Debug("client disconnected: ", slog.Any("userID", userID))
 }
+
+//func (h *NotificationsHandler) GetOldNotifications(w http.ResponseWriter, r *http.Request) {
+//	h.log = h.log.With(
+//		slog.String("op", sl.GFN()),
+//		slog.String("request_id", r.Header.Get(logmw.RequestIDCtx)),
+//	)
+//
+//	userID, ok := r.Context().Value(authmw.AccessTokenCookieName).(uuid.UUID)
+//	if !ok {
+//		h.log.Error("failed cast uuid from context value")
+//		resp.JSONStatus(w, http.StatusUnauthorized)
+//
+//		return
+//	}
+//
+//	comment, err := h.uc.CreateComment(r.Context(), commentPayload)
+//	if err != nil {
+//		h.log.Error("failed in uc.CreateComment", sl.Err(err))
+//		if errors.Is(err, usecase.ErrManyCommentsToProduct) {
+//			resp.JSON(w, http.StatusRequestEntityTooLarge, &comment)
+//
+//			return
+//		}
+//		resp.JSONStatus(w, http.StatusTooManyRequests)
+//
+//		return
+//	}
+//
+//	h.log.Debug("uc.CreateComment", "comment created", comment)
+//	resp.JSON(w, http.StatusOK, &comment)
+//}
